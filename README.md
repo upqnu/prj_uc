88c9555c620d2287b7f8a8806240845f42687028
upqnu <upgrdrqnu@gmail.com>
2023-12-14 15:56:40 +0900

    Docs : Readme 작성 방식 수정

--- README.md
+++ README.md
@@ -1,20 +1,127 @@
-# prj_uc : Project under Control
-  
-## 프로젝트 관리 및 분석 시스템
-- [개요](#개요)
-- [Skils](#skils)
-- [API Reference](#api-reference)
-- [ERD](#ERD)
+# :pushpin: prj_uc : Project under Control
+> **Proejct under Contorl! 프로젝트 관리 및 분석을 한 번에!**  
+> 
+> 프로젝트 및 작업 관리를 통해 협업 및 업무 효율성을 향상을 추구하는 Spring MVC 기반 REST API 서비스
+
+## <포트폴리오 순서>
+1. [제작기간 및 참여인원](#1.제작기간 및 참여인원)  
+2. [개요](#2.개요)  
+3. [사용기술](#3.사용기술)  
+4. [ERD 설계](#4.ERD 설계)
+5. [핵심기능](#5.핵심기능)
+6. [중요 트러블 슈팅](#6.중요 트러블 슈팅)
+7. [기타 트러블 슈팅](#7.기타 트러블 슈팅)
 - [프로젝트 진행관리](#프로젝트-진행관리)
 - [Git commit 메시지 컨벤션](#Git-commit-메시지-컨벤션)
+- [API Reference](#api-reference)
   
-## 개요
-**Proejct under Contorl! 프로젝트 관리 및 분석을 한 번에!** 본 서비스는 기업 내부에서 사용되는 웹 서비스로, 프로젝트 및 작업 관리를 위한 직관적이고 유연한 도구로, 팀 간 협업과 업무 효율성을 향상 시키는데 사용됩니다. <br/>
+## 1.제작기간 및 참여인원
+- 2023년 11월 16일 ~ 11월 24일
+- 개인 프로젝트
   
-## Skils
-언어 및 프레임워크: ![Static Badge](https://img.shields.io/badge/Java-17-darkgreen) ![Static Badge](https://img.shields.io/badge/Spring_boot-REST-darkgreen)<br/>
-데이터베이스 및 테스트: ![Static Badge](https://img.shields.io/badge/h2-2.1.214-blue) ![Static Badge](https://img.shields.io/badge/JUnit-red) <br/>
-배포 : ![Static Badge](https://img.shields.io/badge/Gradle-039BC6) <br/>
+## 2.개요
+**Proejct under Contorl! 프로젝트 관리 및 분석을 한 번에!**  
+본 서비스는 기업 내부에서 사용되는 웹 서비스로, 프로젝트 및 작업 관리를 위한 직관적이고 유연한 도구로, 팀 간 협업과 업무 효율성을 향상 시키는데 사용됩니다. <br/>
+  
+## 3.사용기술
+- 언어 및 프레임워크: ![Static Badge](https://img.shields.io/badge/Java-17-F58232) ![Static Badge](https://img.shields.io/badge/Spring_boot-3.1.1-6CB52C)<br/>
+- 데이터베이스 및 테스트: ![Static Badge](https://img.shields.io/badge/h2-2.1.214-1021FF) ![Static Badge](https://img.shields.io/badge/Spring_Data_JPA-3.1.1-80E96E) ![Static Badge](https://img.shields.io/badge/JUnit-5.9.3-3F9B61)<br/>
+- 보안, 배포 : ![Static Badge](https://img.shields.io/badge/Spring_Security-6.1.1-80E96E) ![Static Badge](https://img.shields.io/badge/Gradle-8.1.1-02303A) <br/>
+  
+## 4.ERD 설계
+> ![image](https://github.com/upqnu/prj_uc/assets/101033614/fdcfc301-b0a5-44c2-bd16-2b684f2e277f)
+  
+<details>
+<summary>ERD 설계 중요포인트 </summary>
+
+- Member, Team의 관계는 다수 Member가 다수 Team의 구성원이 될 수 있는 N : M이므로 → <u>TeamSetting 엔티티(중간테이블)</u>를 생성하여 Member, Team과의 연관관계를 N:1, M:1 로 설정  
+  
+- TeamSetting 엔티티는 enum 타입의 InviteStatus를 설정하여 팀과 관련된 사용자의 권한을 4가지로 설정하는 역할을 수행  
+   - INVITING ; 팀을 생성한 사용자에게만 주어짐 (팀장 권한)  
+   - RECEIVED ; 팀장이 보낸 팀으로의 초대를 받은 사용자  
+   - ACCEPTED ; 초대를 수락하여 팀원이 된 사용자  
+   - REFUSED ; 초대를 거절한 사용자  
+  
+ - 하나의 팀이 하나의 칸반보드를 가질 수 있음.  
+   - 따라서 칸반보드는 Team 엔티티의 필드로만 설정 (사실상, 칸반보드 = 팀)  
+  
+ - 팀을 만드는 사용자가 해당 팀의 팀장이 됨.  
+   - 팀장만이 팀에 속하는 진행상황(Progress, 칸반보드에서 column)을 생성할 수 있다  
+  
+</details>
+  
+## 5.핵심기능
+
+- 사용자(Member), 팀(Team), 작업상황(Progress), 티켓(Ticket) 대상으로 16개의 API 구현
+  
+<details>
+<summary>1st phase (2023.11.16~11.24) ; 사용자 생성 및 JWT를 통한 인증, (프로젝트 진행할) 팀 생성 및 구성, 작업상황 및 티켓 구현을 통해  서비스 구현</summary>
+
+1. 사용자 회원가입 및 <u>**JWT**를 통한 사용자 인증</u> 구현
+    - 사용자 로그인과 동시에 액세스 토큰 발급 후, API 요청 헤더에 담아 사용자 인증
+    - 액세스 토큰 유효시간(30분) 만료 시, 토큰 타입을 “refresh”로 입력하여 유효기간이 1주일로 연장된 새로운 액세스 토큰 발급
+  
+  
+2. 팀 생성, 팀원 초대를 통한 팀 구성
+    - <u>**팀 생성**</u> ; Team 및 TeamSetting 엔티티를 통해 팀의 정보를 구성
+        - Member, Team의 관계는 다수 Member가 다수 Team의 구성원이 될 수 있는 N : M이므로  → TeamSetting 엔티티를 생성하여 Member, Team과의 연관관계를 N : 1, M : 1 로 설정
+            - 관계형 데이터베이스에서 Member, Team 테이블은 연결할 TeamSetting 테이블을 엔티티로 생성한다.
+        - TeamSetting 엔티티는 enum 타입의 InviteStatus를 설정하여 팀과 관련된 사용자의 권한을 4가지로 설정하는 역할을 수행
+            - INVITING ; 팀을 생성한 사용자에게만 주어짐 (팀장 권한)
+            - RECEIVED ; 팀장이 보낸 팀으로의 초대를 받은 사용자
+            - ACCEPTED ; 초대를 수락하여 팀원이 된 사용자
+            - REFUSED ; 초대를 거절한 사용자
+        - 팀 생성 과정 중 팀명 등의 기본 정보는 Team 타입으로 저장되지만, 해당 팀의 구성원 정보는 TeamSetting 타입으로 저장됨.  
+    - <u>**팀원으로 초대 및 수락/거절**</u> ; TeamSetting의 InviteStatus 필드(enum타입) 값에 따라 팀원을 초대, 초대장 수신, 초대를 수락, 초대를 거절한 팀원의 상태를 구분. 따라서 초대와 관련된 DB테이블을 별도로 만들 필요가 없음.
+        - 팀장만이 다른 사용자를 팀원으로 초대할 수 있음
+            - TeamSetting에서 InviteStatus.INVITING인 사용자만 초대가 가능하도록 구현
+        - 특정 사용자가 특정 팀으로의 초대에 대해 수락 또는 거절
+            - TeamSetting에서 InviteStatus.RECEIVED인 사용자만 초대에 대해 수락 또는 거절을 할 수 있음
+            - InviteStatus는 수락 시 ACCEPTED, 거절 시 REFUSED로 변경됨
+        - (본 프로젝트는 REST API 구현하므로, InviteStatus에 따라 초대와 관련된 액션을 구현하는 것은 프런트엔드에서 처리 가능)
+  
+  
+3. 진행상황(Progress) / 티켓 (Ticket) ; 생성, 조회, 수정, 삭제
+    - 팀 내부에 생성할 수 있는 진행상황(Progress)는 칸반보드에서 column과 같은 역할
+    - 진행상황 내부에 생성할 수 있는 티켓(Ticket)은 칸반보드에서의 개별작업과 같은 역할
+    - 진행상황/티켓 생성과 동시에 순서가 정해지며, 순서는 변경이 가능
+  
+  
+4. 서버 구동과 동시에 dummy data 생성
+    - ApplicationRunner 인터페이스를 구현한 DummyDataLoader 클래스, dummy로 생성되어야 할 객체의 정보들을 입력한 DummyDataService 클래스를 통해 Member, Team, TeamSetting, Progress, Ticket의 dummy data가 각각 20, 4, 15, 2, 5개가 서버 구동과 동시에 생성됨
+</details>
+
+<details>
+<summary>2nd phase (2023.12. 1~현재) ; 메서드 모듈화를 통해 코드 중복 제거, Test code 작성, Swagger 도입</summary>
+
+1. 메서드 모듈화를 통해 코드 중복 제거
+    - 1st phase에서 진행상황(Progress), 티켓(Ticket)의 Service 클래스에 작성된 메서드들은 내부에는 많은 중복이 존재. 이를 별도의 메서드로 작성하여 중복 제거.
+        - 중복되는 로직 : (1) 팀, 진행상황, 티켓의 존재 여부 (2) 로그인한 사용자의 팀내에서의 역할
+        - 중복 로직을 별도의 메서드로 작성하여 제거
+    - 개선 결과 : 총 9개의 비즈니스 로직 담당 메서드 별 최소 5라인, 최대 13라인의 코드 감소 및 가독성 개선
+</details>
+  
+## 6.중요 트러블 슈팅
+
+1. member, team 의 다대다 관계 (1) teamSetting 연결테이블로 풀어보기 (<a href="https://github.com/upqnu/prj_uc/wiki/member,%20team%20%EC%9D%98%20%EB%8B%A4%EB%8C%80%EB%8B%A4%20%EA%B4%80%EA%B3%84%20(1)%20teamSetting%20%EC%97%B0%EA%B2%B0%ED%85%8C%EC%9D%B4%EB%B8%94%EB%A1%9C%20%ED%92%80%EC%96%B4%EB%B3%B4%EA%B8%B0">클릭 & detail 확인</a>)
+  
+2. member, team 의 다대다 관계 (2) teamSetting으로 초대 관련 기능을 쉽게 구현 (<a href="https://github.com/upqnu/prj_uc/wiki/member,%20team%20%EC%9D%98%20%EB%8B%A4%EB%8C%80%EB%8B%A4%20%EA%B4%80%EA%B3%84%20(2)%20teamSetting%EC%9C%BC%EB%A1%9C%20%EC%B4%88%EB%8C%80%20%EA%B4%80%EB%A0%A8%20%EA%B8%B0%EB%8A%A5%EC%9D%84%20%EC%89%BD%EA%B2%8C%20%EA%B5%AC%ED%98%84">클릭 & detail 확인</a>)
+  
+## 7.기타 트러블 슈팅
+  
+---
+  
+## 프로젝트 진행 관리
+![image](https://github.com/upqnu/readme_tester/assets/101033614/832372c4-5a3d-4d66-9f14-c423b91a021c)
+  
+## Git commit 메시지 컨벤션
+- `Feat` : 새로운 기능 추가
+- `Fix` : 기능 개선 또는 수정, 버그 수정
+- `Docs` : 문서 수정
+- `Style` : 코드 포맷팅, 세미콜론 누락, 코드 변경이 없는 경우
+- `Refactor` : 코드 리펙토링
+- `Test` : 테스트 코드, 리펙토링 테스트 코드 추가
+- `Chore` : 빌드 업무 수정, 패키지 매니저 수정
   
 ## API Reference
 ### _Member_
@@ -684,22 +791,5 @@ Content-Type: application/json
 ```
 </details>
   
-## ERD
-> ![image](https://github.com/upqnu/prj_uc/assets/101033614/fdcfc301-b0a5-44c2-bd16-2b684f2e277f)
-> - 특정 멤버(member)가 어떤 팀(team)에서는 팀장, 다른 팀에서는 팀원의 역할을 할 수 있다. 그래서 member : team = n : m의 관계이다.
->   - 따라서 TeamSetting을 member, team 사이에 두어 n:m 관계를 1:n, m:1 관계로 나누면서
->   - TeamSetting의 InviteStatus 필드의 값에 따라 팀장, 팀원 여부를 판단하게 하였다.
-> - 하나의 팀이 하나의 칸반보드를 가질 수 있다. 따라서 칸반보드는 Team 엔티티의 필드로만 설정하였다. (사실상 칸반보드 = 팀의 개념)
-> - 팀을 만드는 사용자가 해당 팀의 팀장이 된다. 팀장만이 팀에 속하는 진행상황(Progress, 칸반보드에서 column)을 생성할 수 있다.
-  
-## 프로젝트 진행 관리
-![image](https://github.com/upqnu/readme_tester/assets/101033614/832372c4-5a3d-4d66-9f14-c423b91a021c)
-  
-## Git commit 메시지 컨벤션
-- `Feat` : 새로운 기능 추가
-- `Fix` : 기능 개선 또는 수정, 버그 수정
-- `Docs` : 문서 수정
-- `Style` : 코드 포맷팅, 세미콜론 누락, 코드 변경이 없는 경우
-- `Refactor` : 코드 리펙토링
-- `Test` : 테스트 코드, 리펙토링 테스트 코드 추가
-- `Chore` : 빌드 업무 수정, 패키지 매니저 수정
+
+
--- src/test/java/pp/rsmmm/domain/team/controller/TeamControllerTest.java
+++ src/test/java/pp/rsmmm/domain/team/controller/TeamControllerTest.java
@@ -1,7 +1,9 @@
 package pp.rsmmm.domain.team.controller;
 
+import jakarta.persistence.EntityNotFoundException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
@@ -13,6 +15,8 @@ import pp.rsmmm.domain.member.dto.SignUpRequestDto;
 import pp.rsmmm.domain.member.entity.Member;
 import pp.rsmmm.domain.member.repository.MemberRepository;
 import pp.rsmmm.domain.team.dto.TeamCreateRequestDto;
+import pp.rsmmm.domain.team.entity.Team;
+import pp.rsmmm.domain.team.repository.TeamRepository;
 import pp.rsmmm.global.config.jwt.TokenProvider;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -34,11 +38,128 @@ class TeamControllerTest extends IntegrationTest {
     @Autowired
     TokenProvider tokenProvider;
 
-    private HttpHeaders headers;
+    @Autowired
+    TeamRepository teamRepository;
+
+    private static HttpHeaders headers;
+
+    private Member teamLeader;
+    private Member teamMate;
+    private Member notTeamMember;
 
     @BeforeEach
-    void getAccessToken() throws Exception {
-        Member member = createMember();
+    void setUp() throws Exception {
+        teamLeader = createMember("teamLeader");
+        teamMate = createMember("teamMate");
+        notTeamMember = createMember("notTeamMember");
+        getAccessToken(teamLeader);
+    }
+
+    @DisplayName("팀 생성 - 성공")
+    @Test
+    public void createTeam_succeed() throws Exception {
+        // given
+        TeamCreateRequestDto teamCreateRequestDto = new TeamCreateRequestDto("testTeam", "testKanban");
+        System.out.println("<teamCreateRequestDto>" + String.valueOf(teamCreateRequestDto));
+
+        // when
+        mvc.perform(post("/api/teams/create")
+                .headers(headers)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(teamCreateRequestDto))
+                )
+                // then
+                .andDo(print())
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.status").exists())
+                .andExpect(jsonPath("$.message").exists());
+
+        Team team = teamRepository.findById(1L)
+                .orElseThrow(() -> new EntityNotFoundException("팀이 읎다...")); // test에서 생성된 팀은 1개여서 teamId = 1L 이다. 실제 1L이라는 id를 가진 팀의 존재여부를 확인.
+
+        System.out.println(team.getName());
+    }
+
+    @DisplayName("팀원 초대1 - 성공")
+    @Test
+    public void inviteMate1_succeed() throws Exception {
+        // given
+        String invitedMemberName = teamMate.getName();
+        System.out.println("<이름> : " + invitedMemberName);
+
+        Long teamId = 1L;
+        Team team = teamRepository.findById(1L)
+                .orElseThrow(() -> new EntityNotFoundException("팀이 읎다...")); // test에서 생성된 팀은 1개여서 teamId = 1L 이다. 실제 1L이라는 id를 가진 팀의 존재여부를 확인.
+        System.out.println("<team> : " + String.valueOf(team));
+
+        // when
+        mvc.perform(post("/api/teams/" + teamId + "/invite")
+                .headers(headers)
+                .param("invitedMemberName", invitedMemberName)
+                )
+                // then
+                .andDo(print())
+                .andExpect(status().isOk());
+    }
+
+    @DisplayName("팀원 초대2 - 성공")
+    @Test
+    public void inviteMate2_succeed() throws Exception {
+        // given
+        String invitedMemberName2 = notTeamMember.getName();
+        Long teamId = 1L;
+
+        // when
+        mvc.perform(post("/api/teams/" + teamId + "/invite")
+                        .headers(headers)
+                        .param("invitedMemberName", invitedMemberName2)
+                )
+                // then
+                .andDo(print())
+                .andExpect(status().isOk());
+    }
+
+    @DisplayName("팀원 초대에 수락 - 성공")
+    @Test
+    public void respondToInvitationTest() throws Exception {
+        // given
+        getAccessToken(teamMate);
+        Long teamId = 1L;
+        Team team = teamRepository.findById(1L)
+                .orElseThrow(() -> new EntityNotFoundException("팀이 읎다...")); // test에서 생성된 팀은 1개여서 teamId = 1L 이다. 실제 1L이라는 id를 가진 팀의 존재여부를 확인.
+        System.out.println("<team> : " + String.valueOf(team));
+
+        Long inviteeId = teamMate.getId();
+        Member member = memberRepository.findById(inviteeId)
+                .orElseThrow(() -> new EntityNotFoundException("멤버가 없다"));
+        System.out.println("<member> : " + String.valueOf(member));
+
+        boolean accept = true;
+
+        // when
+        mvc.perform(post("/api/teams/" + teamId + "/invitation/" + inviteeId)
+                .headers(headers)
+                .param("accept", String.valueOf(accept))
+                )
+                // then
+                .andDo(print())
+                .andExpect(status().isOk());
+    }
+
+//    @Test
+//    public void getTeamTest() throws Exception {
+//        // given
+//        Long teamId = 1L;
+//
+//        // when
+//        ResultActions resultActions = mvc.perform(get("/api/teams/" + teamId));
+//
+//        // then
+//        resultActions.andExpect(status().isOk());
+//    }
+
+
+    void getAccessToken(Member member) throws Exception {
         System.out.println("[member] : " + String.valueOf(member));
         String tokenType = "access";
         String accessToken = tokenProvider.issueToken(member, tokenType);
@@ -52,169 +173,48 @@ class TeamControllerTest extends IntegrationTest {
         headers.setContentType(MediaType.APPLICATION_JSON);
         headers.set("Authorization", "Bearer " + accessToken);  // Bearer는 토큰 타입이며, 필요에 따라 다르게 설정할 수 있다.
         System.out.println("[Headers] ; " + String.valueOf(headers));
-//        return headers;
     }
 
-    Member createMember() {
-        Member member = Member.builder()
-                .name("tokenTester")
-                .password(passwordEncoder.encode("zxcv1234"))
-                .email("tokenTester@email.com")
-                .authority(ROLE_MEMBER)
-                .build();
+    private Member createMember(String memberPosition) {
+        if (memberPosition.equals("teamLeader")) {
+            teamLeader = Member.builder()
+                    .name("teamLeader")
+                    .password(passwordEncoder.encode("zxcv1234"))
+                    .email("teamLeader@email.com")
+                    .authority(ROLE_MEMBER)
+                    .build();
 
-        memberRepository.save(member);
+            memberRepository.save(teamLeader);
 
-        return member;
-    }
+            return teamLeader;
+        }
 
-    @DisplayName("팀 생성 - 성공")
-    @Test
-    public void createTeam_succeed() throws Exception {
-        // given
-        TeamCreateRequestDto teamCreateRequestDto = new TeamCreateRequestDto("testTeam", "testKanban");
-        System.out.println("<teamCreateRequestDto>" + String.valueOf(teamCreateRequestDto));
+        if (memberPosition.equals("teamMate")) {
+            teamMate = Member.builder()
+                    .name("teamMate")
+                    .password(passwordEncoder.encode("zxcv1234"))
+                    .email("teamMate@email.com")
+                    .authority(ROLE_MEMBER)
+                    .build();
 
-        // when
-//        HttpHeaders headers = getAccessToken();
+            memberRepository.save(teamMate);
 
-        mvc.perform(post("/api/teams/create")
-                .headers(headers)
-                .contentType(MediaType.APPLICATION_JSON)
-                .content(objectMapper.writeValueAsString(teamCreateRequestDto))
-                )
-                // then
-                .andDo(print())
-                .andExpect(status().isCreated())
-                .andExpect(jsonPath("$.status").exists())
-                .andExpect(jsonPath("$.message").exists());
+            return teamMate;
+        }
 
-        System.out.println("[team]");
-    }
+        if (memberPosition.equals("notTeamMember")) {
+            notTeamMember = Member.builder()
+                    .name("notTeamMember")
+                    .password(passwordEncoder.encode("zxcv1234"))
+                    .email("notTeamMember@email.com")
+                    .authority(ROLE_MEMBER)
+                    .build();
 
-    @Test
-    public void getTeamTest() throws Exception {
-        // given
-        Long teamId = 1L;
+            memberRepository.save(notTeamMember);
 
-        // when
-        ResultActions resultActions = mvc.perform(get("/api/teams/" + teamId));
+            return notTeamMember;
+        }
 
-        // then
-        resultActions.andExpect(status().isOk());
-    }
-
-    @Test
-    public void inviteMateTest() throws Exception {
-        // given
-        Long teamId = 1L;
-        String invitedMemberName = "testMember";
-
-        // when
-        ResultActions resultActions = mvc.perform(post("/api/teams/" + teamId + "/invite")
-                .param("invitedMemberName", invitedMemberName));
-
-        // then
-        resultActions.andExpect(status().isOk());
-    }
-
-    @Test
-    public void respondToInvitationTest() throws Exception {
-        // given
-        Long teamId = 1L;
-        Long inviteeId = 1L;
-        boolean accept = true;
-
-        // when
-        ResultActions resultActions = mvc.perform(post("/api/teams/" + teamId + "/invitation/" + inviteeId)
-                .param("accept", String.valueOf(accept)));
-
-        // then
-        resultActions.andExpect(status().isOk());
+        return null;
     }
 }
-
-//    @BeforeEach
-//    void init() {
-//        Member member = Member.builder()
-//                .name("teamTester")
-//                .password(passwordEncoder.encode("vcxz1234"))
-//                .email("teamTester@email.com")
-//                .authority(ROLE_MEMBER)
-//                .build();
-//
-//        memberRepository.save(member);
-//
-//        String tokenType = "access";
-//        String token = tokenProvider.issueToken(member, tokenType);
-//    }
-//
-//    @DisplayName("팀(Team) 생성 및 팀구성(TeamSetting) 생성 - 성공")
-//    @Test
-//    void createTeam_succeed() throws Exception {
-//        // given
-//        String token = mvc.perform(MockMvcRequestBuilders.post("/api/members/sign-in")
-//                .contentType(MediaType.APPLICATION_JSON)
-//                .content("{\"username\":\"teamTester\",\"password\":\"vcxz1234\"}"))
-//                .andExpect(MockMvcResultMatchers.status().isOk())
-//                .andReturn().getResponse().getHeader("Authorization");
-//
-//        assert token != null;
-//
-//        // (2) team 생성을 위한 입력
-//        String name = "testTeam";
-//        String kanban = "testKanban";
-//        TeamCreateRequestDto teamCreateRequestDto = TeamCreateRequestDto.of(name, kanban);
-//
-//        // when
-//        mvc.perform(MockMvcRequestBuilders.post("/api/teams/create")
-//                        .header("Authorization", "Bearer " + token)
-//                        .contentType(MediaType.APPLICATION_JSON)
-//                        .content(objectMapper.writeValueAsString(teamCreateRequestDto))
-//                        .andExpect(MockMvcResultMatchers.status().isCreated())
-//        );
-//
-//    }
-//
-//    Member createMember() {
-//        Member member = Member.builder()
-//                .name("teamTester")
-//                .password(passwordEncoder.encode("vcxz1234"))
-//                .email("teamTester@email.com")
-//                .authority(ROLE_MEMBER)
-//                .build();
-//
-//        memberRepository.save(member);
-//
-//        return member;
-//    }
-//
-//    @Test
-//    @Order(1)
-//    String singIn_pre() throws Exception {
-//        // given
-//        Member member = Member.builder()
-//                .name("kanban_tester")
-//                .password(passwordEncoder.encode("asdf4321"))
-//                .email("kanban1@email.com")
-//                .authority(Authority.ROLE_MEMBER)
-//                .build();
-//
-//        memberRepository.save(member);
-//
-//        SignInRequestDto signInRequestDto = SignInRequestDto.of(member.getName(), "asdf4321");
-//
-//        // when
-//        mvc.perform(
-//                        post("/api/members/sign-in")
-//                                .contentType(MediaType.APPLICATION_JSON)
-//                                .content(objectMapper.writeValueAsString(signInRequestDto))
-//                )
-//                // then
-//                .andDo(print())
-//                .andExpect(jsonPath("$.accessToken").isNotEmpty());
-//
-//        return jsonPath("$.accessToken").toString();
-//    }
-
-//}
