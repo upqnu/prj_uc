a7d24b2d7a7479c49430f9b35248a261601e9784
upqnu <upgrdrqnu@gmail.com>
2024-01-10 23:24:47 +0900

    Docs : Swagger 의존성 주입 & Swagger에 API 역할 및 세부정보 입력 - application.yml에 springdoc 설정 & SwaggerConfig 클래스 없이, 스프링 부트 3.0 이상에서 springdoc으로 Swagger 설정 시 build.gradle에 의존성만 입력하면 Swagger 사용가능 - 4개 Controller 및 그에 속하는 API의 역할 및 세부정보 입력

--- src/main/java/pp/rsmmm/domain/member/controller/MemberController.java
+++ src/main/java/pp/rsmmm/domain/member/controller/MemberController.java
@@ -1,5 +1,8 @@
 package pp.rsmmm.domain.member.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -18,6 +21,7 @@ import pp.rsmmm.domain.member.dto.SignUpResponseDto;
 @RestController
 @RequestMapping("/api/members")
 @RequiredArgsConstructor
+@Tag(name = "Member", description = "Member API")
 public class MemberController {
 
     private final MemberService memberService;
@@ -27,6 +31,8 @@ public class MemberController {
      * @param signUpRequestDto
      * @return
      */
+    @Operation(summary = "회원 가입 요청", description = "입력된 회원 정보를 저장합니다")
+    @Parameter(name = "signUpRequestDto", description = "회원 가입 시, name/password/email 필수 입력")
     @PostMapping("/sign-up")
     public ResponseEntity<SignUpResponseDto> signUp(@RequestBody SignUpRequestDto signUpRequestDto) {
         SignUpResponseDto signUpResponseDto = memberService.signUp(signUpRequestDto);
@@ -38,6 +44,8 @@ public class MemberController {
      * @param signInRequestDto
      * @return
      */
+    @Operation(summary = "로그인", description = "로그인 & 액세스 토큰 발급")
+    @Parameter(name = "signInRequestDto", description = "memberName/password 입력하여 로그인")
     @PostMapping("/sign-in")
     public ResponseEntity<TokenResponseDto> singIn(@RequestBody @Valid SignInRequestDto signInRequestDto) {
         return ResponseEntity.ok(memberService.signIn(signInRequestDto));
@@ -48,6 +56,8 @@ public class MemberController {
      * @param renewTokenRequestDto
      * @return
      */
+    @Operation(summary = "토큰 재발급", description = "유효기간 만료 시, 액세스 토큰 재발급")
+    @Parameter(name = "RenewTokenRequestDto", description = "'refreshToken' 입력하여 액세스 토큰 재발급")
     @PostMapping("/refresh")
     public ResponseEntity<RenewTokenResponseDto> renewToken(@RequestBody RenewTokenRequestDto renewTokenRequestDto) {
         String renewedAccessToken = memberService.renewToken(renewTokenRequestDto.getRefreshToken());
